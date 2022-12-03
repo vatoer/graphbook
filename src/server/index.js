a57bf5961afe6463a1f1da25c9cf82f896ad8bf3
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-03 22:53:27 +0200

    Routing in Express.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,5 +2,11 @@ import express  from "express";
 
 const app = express();
 
-app.get('*', (req, res) => res.send('hello wewew') );
+app.get('/', function (req, res, next){
+    console.log('1st function');
+    next();
+}, function (req, res) {
+    console.log('2nd function');
+    res.send('hellooo');
+} );
 app.listen(8000, () => console.log('listening on port 8000'));
