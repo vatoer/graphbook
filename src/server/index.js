8f53b552e2f0c4c629d75929fd423c324efd9bbe
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-03 23:41:15 +0200

    Express Helmet

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,22 @@
 import express  from "express";
 import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compres from 'compression';
 
 const app = express();
 const root = path.join(__dirname, '../../');
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'","'unsafe-inline'"],
+        imgSrc: ["'self'","data:", "*.amazonaws.com"]
+    }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin'}));
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'public/uploads')));
