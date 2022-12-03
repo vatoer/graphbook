5e2cd111fb7d05914865404e302321e95f6aba17
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-03 17:22:50 +0200

    Rendering arrays from React state

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialPost = [
+    {
+        id: 2,
+        text: 'lorem ipsum',
+        user: {
+            avatar: '/uploads/avatar1.png',
+            username: 'test user'
+        }
+    },
+    {
+        id: 1,
+        text: 'Lorem ipsum',
+        user: {
+            avatar: '/uploads/avatar2.png',
+            username: 'Test User 2'
+        }
+    }
+];
 
 const App = () => {
+    const [post, setPosts] = useState(initialPost);
+    
     return (
-        <div>Hellow </div>
+        <div className='container'>
+            <div className='feed'>
+                { initialPost.map( (post, i) => 
+                    <div key= {post.id} className="post" >
+                        <div className='header'>
+                            <img src={post.user.avatar} />
+                            <h2>{post.user.username}</h2>
+                        </div>
+                        <p className='content'>
+                            {post.text}
+                        </p>
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
 
