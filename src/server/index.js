67b9c0e8473420c65542af491e65aca25070747d
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-03 23:04:32 +0200

    Serving our production build

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,12 @@
 import express  from "express";
+import path from 'path';
 
 const app = express();
+const root = path.join(__dirname, '../../');
 
-app.get('/', function (req, res, next){
-    console.log('1st function');
-    next();
-}, function (req, res) {
-    console.log('2nd function');
-    res.send('hellooo');
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'public/uploads')));
+app.get('/', (req, res) => {
+    res.send(path.join, '/dist/client/index.html');
 } );
 app.listen(8000, () => console.log('listening on port 8000'));
