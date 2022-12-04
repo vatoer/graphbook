4b6828067ab716c2c4b9ed8b0561b703242eaf31
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-04 11:07:28 +0200

    schema and resolvers

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,26 @@
-import express  from "express";
+import express from "express";
 import path from 'path';
 import helmet from 'helmet';
 import cors from 'cors';
 import compres from 'compression';
+import services from "./services";
 
 const app = express();
 const root = path.join(__dirname, '../../');
 
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'","'unsafe-inline'"],
-        imgSrc: ["'self'","data:", "*.amazonaws.com"]
-    }
-}));
-app.use(helmet.referrerPolicy({ policy: 'same-origin'}));
+if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+    app.use(helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
+            styleSrc: ["'self'", "'unsafe-inline'"],
+            imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+        }
+    }));
+    app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+}
+
 
 app.use(compres());
 
@@ -26,5 +30,19 @@ app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'public/uploads')));
 app.get('/', (req, res) => {
     res.send(path.join, '/dist/client/index.html');
-} );
+});
+
+const serviceNames = Object.keys(services);
+for (let i = 0; i < serviceNames.length; i += 1) {
+    const name = serviceNames[i];
+    if (name === 'graphql') {
+        (async () => {
+            await services[name].start();
+            services[name].applyMiddleware({ app });
+        })()
+    } else {
+        app.use('/${name}', services[name]);
+    }
+}
+
 app.listen(8000, () => console.log('listening on port 8000'));
