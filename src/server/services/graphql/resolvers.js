02bd41b5a0f6a8b6b63bdbe7edbeff232f728877
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-04 11:17:58 +0200

    Sending GraphQL queries

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,7 +1,26 @@
+const posts = [
+    {
+        id: 2,
+        text: 'lorem ipsum',
+        user: {
+            avatar: '/uploads/avatar1.png',
+            username: 'test user'
+        }
+    },
+    {
+        id: 1,
+        text: 'Lorem ipsum',
+        user: {
+            avatar: '/uploads/avatar2.png',
+            username: 'Test User 2'
+        }
+    }
+];
+
 const resolvers = {
     RootQuery: {
         posts(root, args, context) {
-            return [];
+            return posts;
         },
     },
 };
