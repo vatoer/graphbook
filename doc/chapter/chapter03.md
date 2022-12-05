7d74f8d886ea4bd335e53517eb760aeb0764ddfa
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-05 22:53:42 +0200

    Seeding data with Sequelize

--- doc/chapter/chapter03.md
+++ doc/chapter/chapter03.md
@@ -8,4 +8,9 @@ sequelize model:generate --models-path src/server/models --migrations-path src/s
 sequelize db:migrate --migrations-path src/server/migrations --config src/server/config/index.js
 
 npm install --save-dev babel-plugin-require-context-hook
+
+sequelize seed:generate --name fake-post --seeders-path .\src\server\seeders\
+
+sequelize db:seed:all --seeders-path src/server/seeders --config src/server/config/index.js
+
 ```
---
+++ src/server/seeders/20221205203716-fake-post.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Posts', [
+      {
+        text: 'lorem ipsum 1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        text: 'lorem ipsum 2',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        text: 'lorem ipsum 3',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Posts', null, {});
+  }
+};
