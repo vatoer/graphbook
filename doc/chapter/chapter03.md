dff25f840e8c357fcea247f43ca46f4e53f94f97
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-05 23:35:17 +0200

    Updating the table structure with migrations

--- doc/chapter/chapter03.md
+++ doc/chapter/chapter03.md
@@ -15,4 +15,20 @@ sequelize db:seed:all --seeders-path src/server/seeders --config src/server/conf
 
 sequelize model:generate --models-path src/server/models --migrations-path src/server/migrations --name User --attributes avatar:string,username:string
 
+sequelize migration:create --migrations-path src/server/migrations --name add-userId-to-post
+
+sequelize db:migrate --migrations-path src/server/migrations --config src/server/config/index.js
+
+```
+
+on error migration 
+```
+sequelize db:migrate:undo --migrations-path src/server/migrations --config src/server/config/index.js
+```
+
+You can also revert all migrations at once, or only revert to one specific migration so that
+you can go back to a specific timestamp:
+
+```
+sequelize db:migrate:undo:all --to XXXXXXXXXXXXXX-create-posts.js --migrations-path src/server/migrations --config src/server/config/index.js
 ```
---
+++ src/server/migrations/20221205211623-add-userId-to-post.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn('Posts', 'userId', {
+        type: Sequelize.INTEGER,
+      }),
+      queryInterface.addConstraint('Posts', {
+        fields: ['userId'],
+        type: 'foreign key',
+        name: 'fk_user_id',
+        references: {
+          table: 'Users',
+          field: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      }),
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([
+      queryInterface.removeColumn('Posts', 'userId'),
+    ]);
+  }
+};
