986f0816a03f5c5d562e4e621b0fbef97cbefca8
Fathur Rohman <fathur.rohman@kemlu.go.id>
2022-12-10 14:18:13 +0200

    Installing Apollo Client

---
+++ src/client/apollo/index.js
@@ -0,0 +1,25 @@
+import { ApolloClient, InmMemoryCache, from, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const client = new ApolloClient({
+    link: from([
+        onError(({ graphQLErrors, networkError }) => {
+            if (graphQLErrors) {
+                graphQLErrors.map(({ message, locations, path }) =>
+                    console.log(`
+                    [GraphQL error]: Message: ${message},
+                    Location: 
+                    ${locations}, Path: ${path}`));
+                if (networkError) {
+                    console.log(`[Network error]: ${networkError}`);
+                }
+            }
+        }),
+        new HttpLink({
+            uri: 'http://127.0.0.1:8000/graphql'
+        })
+    ]),
+    cache: new InMemoryCache(),
+});
+
+export default client;
